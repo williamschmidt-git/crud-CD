9e4bb03ec817e763b86e3af2c83c08abc8784d22
William Schmidt <9williamschmidt@gmail.com>
2022-07-02 16:17:23 -0300

    switching ORMs

---
+++ app/backend/src/database/config/database.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER || 'trybe', //root
+  password: process.env.DB_PASS || '@Trybe12345', //123456
+  database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+};
+
+export default config;
---
+++ app/backend/src/database/models/Produtos.ts
@@ -0,0 +1,3 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
