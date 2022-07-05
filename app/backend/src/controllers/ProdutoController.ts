6f71d4e4611d9f0c67526e4fcbd34956cd90cc96
William Schmidt <9williamschmidt@gmail.com>
2022-07-05 17:39:07 -0300

    ✨ feat: findAll controller method

--- app/backend/src/controllers/ProdutoController.ts
+++ app/backend/src/controllers/ProdutoController.ts
@@ -8,6 +8,14 @@ class ProdutoController implements Icontroller {
   constructor(serviceInstance: any) {
     this._service = serviceInstance;
   }
+
+  public findAll = async (_req: Request, res: Response): Promise<Response> => {
+    const response = await this._service.findAll();
+
+    if (response.error) return res.status(404).end();
+
+    return res.status(200).json(response);
+  };
 }
 
 export default ProdutoController;
