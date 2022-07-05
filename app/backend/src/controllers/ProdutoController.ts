13d0079061a4aa1c019b4afd7d9b117974b2f2f8
William Schmidt <9williamschmidt@gmail.com>
2022-07-05 17:41:04 -0300

    ✨ feat: delete controller add

--- app/backend/src/controllers/ProdutoController.ts
+++ app/backend/src/controllers/ProdutoController.ts
@@ -36,6 +36,16 @@ class ProdutoController implements Icontroller {
 
     return res.status(200).json({ message: 'Cliente Atualizado' });
   };
+
+  public delete = async (req: Request, res: Response):Promise<Response> => {
+    const { id } = req.params;
+
+    const response = await this._service.delete(id);
+
+    if (response.error) return res.status(404).json({ error: response.error });
+
+    return res.status(200).json({ message: 'Cliente deletado' });
+  };
 }
 
 export default ProdutoController;
