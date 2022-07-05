85aa6295eff079996bbe5594b6822ea4aeac3594
William Schmidt <9williamschmidt@gmail.com>
2022-07-05 17:40:07 -0300

    ✨ feat: findByName controller add

--- app/backend/src/controllers/ProdutoController.ts
+++ app/backend/src/controllers/ProdutoController.ts
@@ -16,6 +16,15 @@ class ProdutoController implements Icontroller {
 
     return res.status(200).json(response);
   };
+
+  public findByName = async (req: Request, res: Response):Promise<Response> => {
+    const { desc } = req.query;
+    const response = await this._service.findByName(desc);
+
+    if (response.error) return res.status(404).end();
+
+    return res.status(200).json(response);
+  };
 }
 
 export default ProdutoController;
