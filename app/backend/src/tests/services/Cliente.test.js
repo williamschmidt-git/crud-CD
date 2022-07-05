beb61642c515edcd4d8100a056e37e39b93285f2
William Schmidt <9williamschmidt@gmail.com>
2022-07-05 12:14:39 -0300

    ✅ add: update service test

--- app/backend/src/tests/services/Cliente.test.js
+++ app/backend/src/tests/services/Cliente.test.js
@@ -4,7 +4,7 @@ import { describe } from 'mocha'
 import ClienteService from '../../services/ClienteService';
 import ClienteModel from '../../database/models/Cliente';
 import { FIND_ALL_CLIENTES_MOCK } from './mocks/Clientes.mock'
-import { CLIENTE1_MOCK } from '../controllers/mocks/Clientes.mock';
+import { CLIENTE1_MOCK, CLIENTE_CRIADO_MOCK } from '../controllers/mocks/Clientes.mock';
 
 const { expect } = chai;
 
@@ -50,7 +50,7 @@ describe.only('Cliente Service', () => {
       (ClienteModel.findAll).restore();
     });
 
-    it('Deve chamar a função "findByName" e retornar um array', async () => {
+    it('Deve chamar a função "findByName" e retornar um Objeto', async () => {
       sinon.stub(ClienteModel, 'findAll').resolves(CLIENTE1_MOCK)
 
       const response = await service.findByName('Cli1');
@@ -62,12 +62,12 @@ describe.only('Cliente Service', () => {
     });
   });
 
-  describe('findByName endpoint. Em caso de sucesso:', () => {
+  describe('findByName endpoint. Em caso de falha:', () => {
     afterEach(() => {
       (ClienteModel.findAll).restore();
     });
 
-    it('Deve chamar a função "findByName" e retornar um array', async () => {
+    it('Deve chamar a função "findByName" e retornar um Objeto com mensagem de erro', async () => {
       sinon.stub(ClienteModel, 'findAll').resolves({ error: '"Cliente" não encontrado'})
 
       const response = await service.findByName('william');
@@ -76,6 +76,40 @@ describe.only('Cliente Service', () => {
       expect(ClienteModel.findAll).to.be.a('function');
       expect(response).to.be.an('Object')
       expect(response).to.be.deep.equal({ error: '"Cliente" não encontrado'})
+    });
+  });
+
+  describe('update endpoint. Em caso de sucesso:', () => {
+    afterEach(() => {
+      (ClienteModel.update).restore();
+    });
+
+    it('Deve chamar a função "update" e retornar um array', async () => {
+      sinon.stub(ClienteModel, 'update').resolves([1])
+
+      const response = await service.update('1', CLIENTE_CRIADO_MOCK);
+
+      expect(ClienteModel.update.called).to.be.true;
+      expect(ClienteModel.update).to.be.a('function');
+      expect(response).to.be.an('array')
+      expect(response).to.be.deep.equal([1])
+    });
+  });
+
+  describe('update endpoint. Em caso de falha:', () => {
+    afterEach(() => {
+      (ClienteModel.update).restore();
+    });
+
+    it('Deve chamar a função "update" e retornar um array', async () => {
+      sinon.stub(ClienteModel, 'update').resolves({ error: '"Cliente" não encontrado' })
+
+      const response = await service.update('1000', CLIENTE_CRIADO_MOCK);
+
+      expect(ClienteModel.update.called).to.be.false;
+      expect(ClienteModel.update).to.be.a('function');
+      expect(response).to.be.an('object')
+      expect(response).to.be.deep.equal({ error: '"Cliente" não encontrado' })
     });
   });
 });
