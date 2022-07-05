f7e9bb625c0b6c57f03c9d9c534f9da8c9f8c477
William Schmidt <9williamschmidt@gmail.com>
2022-07-05 17:05:23 -0300

    ✨ feat: produtoService created

---
+++ app/backend/src/services/ProdutoService.ts
@@ -0,0 +1,55 @@
+import { Ierror } from './interfaces/Error';
+import { Iproduto } from './interfaces/Produto';
+import { Service } from './interfaces/Service';
+
+class ProdutoService implements Service<Iproduto> {
+  private _model;
+
+  constructor(dbInstance: any) {
+    this._model = dbInstance;
+  }
+
+  public async findAll(): Promise<Iproduto[] | Ierror> {
+    const produtos = await this._model.findAll();
+
+    if (!produtos) return { error: 'Não encontrado' };
+
+    return produtos;
+  }
+
+  public async findByName(name: string): Promise<Iproduto | null | Ierror> {
+    const produto = await this._model.findAll({ where: { dscProduto: name } });
+
+    if (!produto) return { error: 'Não encontrado ' };
+
+    return produto;
+  }
+
+  public async update(id: string, obj: Iproduto): Promise<Iproduto | null | Ierror> {
+    const produto = await this._model.findByPk(id);
+
+    if (!produto) return { error: '"Produto" não encontrado' };
+
+    const produtoAtualizado = await this._model.update({ ...obj }, { where: { idProduto: id } });
+
+    return produtoAtualizado;
+  }
+
+  public async delete(id: string): Promise<Iproduto | null | Ierror> {
+    const produto = await this._model.findByPk(id);
+
+    if (!produto) return { error: '"Produto" não encontrado' };
+
+    const produtoDeletado = await this._model.destroy({ where: { idProduto: id } });
+
+    return produtoDeletado;
+  }
+
+  public async create(obj: Iproduto): Promise<Iproduto | Ierror> {
+    const produto = await this._model.create(obj);
+
+    return produto;
+  }
+}
+
+export default ProdutoService;
