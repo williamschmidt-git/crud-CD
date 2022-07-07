aad32e1bd8ffc9f486ab8d3d7721f83079407bf2
William Schmidt <9williamschmidt@gmail.com>
2022-07-06 21:04:30 -0300

    ✅  add: findBy and update fail/sucess tests

--- app/backend/src/tests/services/Venda.test.js
+++ app/backend/src/tests/services/Venda.test.js
@@ -4,8 +4,13 @@ import { describe } from 'mocha'
 import VendaService from '../../services/VendaService';
 import VendaModel from  '../../database/models/Vendas';
 import { FIND_ALL_VENDAS_MOCK } from './mocks/Vendas.mock';
+import { VENDA1_MOCK, VENDA_CRIADA_MOCK } from '../controllers/mocks/Vendas.mock';
 
 const { expect } = chai;
+
+const mock_test = {
+  desc: 'Produto 1'
+}
 
 describe.only('Venda Service', () => {
   const service = new VendaService(VendaModel);
@@ -41,6 +46,74 @@ describe.only('Venda Service', () => {
       expect(VendaModel.findAll).to.be.a('function');
       expect(response).to.be.an('array')
       expect(response).to.be.deep.equal([])
+    });
+  });
+
+  describe('findBy endpoint. Em caso de sucesso:', () => {
+    afterEach(() => {
+      (VendaModel.findAll).restore();
+    });
+
+    it('Deve chamar a função "findBy" e retornar um array', async () => {
+      sinon.stub(VendaModel, 'findAll').resolves(VENDA1_MOCK)
+
+      const response = await service.findBy(mock_test);
+
+      expect(VendaModel.findAll.called).to.be.true;
+      expect(VendaModel.findAll).to.be.a('function');
+      expect(response).to.be.an('object');
+      expect(response).to.be.deep.equal(VENDA1_MOCK);
+    });
+  });
+
+  describe('findBy endpoint. Em caso de falha:', () => {
+    afterEach(() => {
+      (VendaModel.findAll).restore();
+    });
+
+    it('Deve chamar a função "findBy" e retornar um Objeto com mensagem de erro', async () => {
+      sinon.stub(VendaModel, 'findAll').resolves({ error: '"Produto" não encontrado' })
+
+      const response = await service.findBy({ desc: 'Produto 100000'});
+
+      expect(VendaModel.findAll.called).to.be.true;
+      expect(VendaModel.findAll).to.be.a('function');
+      expect(response).to.be.an('Object')
+      expect(response).to.be.deep.equal({ error: '"Produto" não encontrado' })
+    });
+  });
+
+  describe('update endpoint. Em caso de sucesso:', () => {
+    afterEach(() => {
+      (VendaModel.update).restore();
+    });
+
+    it('Deve chamar a função "update" e retornar um array', async () => {
+      sinon.stub(VendaModel, 'update').resolves([1])
+
+      const response = await service.update('1', VENDA_CRIADA_MOCK);
+
+      expect(VendaModel.update.called).to.be.true;
+      expect(VendaModel.update).to.be.a('function');
+      expect(response).to.be.an('array')
+      expect(response).to.be.deep.equal([1])
+    });
+  });
+
+  describe('update endpoint. Em caso de falha:', () => {
+    afterEach(() => {
+      (VendaModel.update).restore();
+    });
+
+    it('Deve chamar a função "update" e retornar um array', async () => {
+      sinon.stub(VendaModel, 'update').resolves({ error: '"Venda" não encontrada' })
+
+      const response = await service.update('1000', VENDA_CRIADA_MOCK);
+
+      expect(VendaModel.update.called).to.be.false;
+      expect(VendaModel.update).to.be.a('function');
+      expect(response).to.be.an('object');
+      expect(response).to.be.deep.equal({ error: '"Venda" não encontrada' })
     });
   });
 });
