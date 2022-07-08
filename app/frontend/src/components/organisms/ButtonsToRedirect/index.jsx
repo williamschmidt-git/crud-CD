f7bafaecbfe9895154bf8b5771f7c3005eab6a06
William Schmidt <9williamschmidt@gmail.com>
2022-07-07 21:15:52 -0300

    ✨ feat: buttonsToredirect add

--- app/frontend/src/components/organisms/ButtonsToRedirect/index.jsx
+++ app/frontend/src/components/organisms/ButtonsToRedirect/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import Button from '../../atoms/Button';
 
 export default function ButtonsToRedirect() {
+  const navigate = useNavigate();
+
+  function redirectToCustomer() {
+    navigate('/customers');
+  }
+
+  function redirectToProducts() {
+    navigate('/products');
+  }
+
+  function redirectToSales() {
+    navigate('/sales');
+  }
+
   return (
     <div>
-      <Button text="Clientes" />
-      <Button text="Produtos" />
-      <Button text="Vendas" />
+      <Button text="Clientes" onClick={ () => redirectToCustomer() } />
+      <Button text="Produtos" onClick={ () => redirectToProducts() } />
+      <Button text="Vendas" onClick={ () => redirectToSales() } />
     </div>
   );
 }
