6afe3c59a7470a9f71e33c2bce2d2c2bdac3d6a9
William Schmidt <9williamschmidt@gmail.com>
2022-07-08 22:15:42 -0300

    ✨ feat: createUser page + validation JOI

---
+++ app/frontend/src/pages/CreateCustomerPage/index.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import Input from '../../components/atoms/Input';
+import Button from '../../components/atoms/Button';
+import { createCustomer } from '../../http/cliente';
+
+export default function CreateCustomerPage() {
+  const [newCustomer, setNewCustomer] = useState({
+    nmCliente: '',
+    cidade: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createCustomer(newCustomer);
+  };
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setNewCustomer({
+      ...newCustomer,
+      [name]: value,
+    });
+  };
+
+  console.log(newCustomer);
+
+  return (
+    <div>
+      <form onSubmit={ handleSubmit }>
+        <div>
+          <label htmlFor="input-name">
+            Insira o nome:
+            <Input
+              type="text"
+              name="nmCliente"
+              id="input-name"
+              onChange={ (e) => handleChange(e) }
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="input-city">
+            Insira a cidade:
+            <Input
+              type="text"
+              name="cidade"
+              id="input-city"
+              onChange={ (e) => handleChange(e) }
+            />
+          </label>
+        </div>
+        <Button type="submit" text="Send" />
+      </form>
+
+    </div>
+  );
+}
