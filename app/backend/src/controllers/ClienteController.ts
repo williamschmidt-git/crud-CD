fe58911553901af21a88a85ab3d0f5b790f65c58
William Schmidt <9williamschmidt@gmail.com>
2022-07-09 13:05:40 -0300

    ✨ feat: handle uncaught errors

--- app/backend/src/controllers/ClienteController.ts
+++ app/backend/src/controllers/ClienteController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import Icontroller from './interfaces/Controller';
 import Schema from '../schemas/ClienteSchema';
 
@@ -11,55 +11,73 @@ class ClienteController implements Icontroller {
     this.findBy = this.findBy.bind(this);
   }
 
-  public findAll = async (_req: Request, res: Response): Promise<Response> => {
-    const response = await this._service.findAll();
+  public findAll = async (_req: Request, res: Response, next: NextFunction): Promise<Response> => {
+    try {
+      const response = await this._service.findAll();
 
-    if (response.error) return res.status(404).end();
+      if (response.error) return res.status(404).end();
 
-    return res.status(200).json(response);
+      return res.status(200).json(response);
+    } catch (error) {
+      next(error);
+    }
   };
 
-  public findBy = async (req: Request, res: Response):Promise<Response> => {
-    const { name } = req.query;
-    const response = await this._service.findBy(name);
-
-    if (response.error) return res.status(404).end();
-
-    return res.status(200).json(response);
+  public findBy = async (req: Request, res: Response, next: NextFunction):Promise<Response> => {
+    try {
+      const { name } = req.query;
+      const response = await this._service.findBy(name);
+      if (response.error) return res.status(404).end();
+      return res.status(200).json(response);
+    } catch (error) {
+      next(error);
+    }
   }; 
 
-  public update = async (req: Request, res: Response):Promise<Response> => {
-    const { id } = req.params;
-    const obj = req.body;
+  public update = async (req: Request, res: Response, next: NextFunction):Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const obj = req.body;
 
-    const response = await this._service.update(id, obj);
+      const response = await this._service.update(id, obj);
 
-    if (response.error) return res.status(404).json({ error: response.error });
+      if (response.error) return res.status(404).json({ error: response.error });
 
-    return res.status(200).json({ message: 'Cliente Atualizado' });
-  };
-
-  public delete = async (req: Request, res: Response):Promise<Response> => {
-    const { id } = req.params;
-
-    const response = await this._service.delete(id);
-
-    if (response.error) return res.status(404).json({ error: response.error });
-
-    return res.status(200).json({ message: 'Cliente deletado' });
-  };
-
-  public create = async (req: Request, res: Response):Promise<Response> => {
-    const { error } = Schema.validate(req.body);
-
-    if (error) {
-      const [code, message] = error.message.split('|');
-      return res.status(Number(code)).json(message);
+      return res.status(200).json({ message: 'Cliente Atualizado' });
+    } catch (error) {
+      next(error);
     }
+  };
 
-    const response = await this._service.create(req.body);
+  public delete = async (req: Request, res: Response, next: NextFunction):Promise<Response> => {
+    try {
+      const { id } = req.params;
 
-    return res.status(201).json(response);
+      const response = await this._service.delete(id);
+
+      if (response.error) return res.status(404).json({ error: response.error });
+
+      return res.status(200).json({ message: 'Cliente deletado' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public create = async (req: Request, res: Response, next: NextFunction):Promise<Response> => {
+    try {
+      const { error } = Schema.validate(req.body);
+
+      if (error) {
+        const [code, message] = error.message.split('|');
+        return res.status(Number(code)).json(message);
+      }
+
+      const response = await this._service.create(req.body);
+
+      return res.status(201).json(response);
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
